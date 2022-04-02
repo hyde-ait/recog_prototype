dac6ee10c81fc61b9d9c47b3688640922ad3841e
Aitouakli Hichem <alterhichem99@gmail.com>
2022-04-02 09:48:29 +0100

    fixed server

--- server.py
+++ server.py
@@ -144,5 +144,5 @@ if __name__ == "__main__":
         ssl_context = None
     print(ssl_context)
 
-    uvicorn.run('main:app', port=args.port,
+    uvicorn.run('server:app', port=args.port,
                 host=args.host, reload=True)
