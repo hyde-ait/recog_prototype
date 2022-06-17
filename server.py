39ffedb0ed8ac9013de8241be250d6a85ee12753
Aitouakli Hichem <alterhichem99@gmail.com>
2022-06-17 11:34:46 +0100

    origin bug

--- server.py
+++ server.py
@@ -25,13 +25,7 @@ relay = MediaRelay()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "http://127.0.0.1",
-    "*"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
